cac2ed996496aa8765f40524724fc13665b75d3b
JayasreeNaragam <50613022+JayasreeNaragam@users.noreply.github.com>
2019-05-14 13:10:21 +0530

    Add files via upload

---
+++ LED1.c
@@ -0,0 +1,28 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 09-05-2019 14:20:36
+ * Author : Jaya
+ */ 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include<util/delay.h>
+
+
+
+int main(void)
+{
+      DDRB=0xFF;
+while(1)
+{
+   PORTB = 0xFF;
+   _delay_ms(100);
+   PORTB=0x00;
+   _delay_ms(100);
+  
+}
+}
+
